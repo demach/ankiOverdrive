10e4905f4ee9346695ad155153dc92a77e20347d
Dennis Macho <58945027+demach@users.noreply.github.com>
2022-04-08 16:31:18 +0200

    tested and adjusted values

--- fts_track_discovery.py
+++ fts_track_discovery.py
@@ -10,7 +10,7 @@ from py_overdrive_sdk.py_overdrive import Overdrive
 
 # parse input
 parser = argparse.ArgumentParser()
-parser.add_argument("--car", help="id of the bluetooth car")
+parser.add_argument("--car", help="id of the bluetooth car", default="daa87a9810c9")
 parser.add_argument("--host", help="host of the node gateway for bluetooth communication", default='127.0.0.1')
 parser.add_argument("--port", help="port of the node gateway for bluetooth communication", type=int, default=8005)
 args = parser.parse_args()
@@ -20,13 +20,14 @@ TRACK_FILE = 'track_piece_list.txt'
 
 
 # define custom driving policy for track discovery
-def discovery_driving_policy(self, **kwargs):
-    def clockwise_format(bool_str):
+def discovery_driving_policy(self, **kwargs) -> None:
+    def clockwise_format(bool_str) -> str:
         if bool_str.lower() == "true":
             return "cw_1"
         else:
             return "cw_0"
-    def create_trackfile_insert(track_pieces_lst):
+    
+    def create_trackfile_insert(track_pieces_lst) -> str:
         write_lst = []
         for p in track_pieces_lst:
             write_lst.append('{},{}'.format(p[0], p[2]))
@@ -37,52 +38,62 @@ def discovery_driving_policy(self, **kwargs):
     if not hasattr(self, 'progress'):
         self.progress = 0
     if not hasattr(self, 'last_abspos'):
-        self.last_abspos = (int(kwargs["piece"]), int(kwargs["location"]), kwargs["is_clockwise"], "dummy_element")
+        self.last_abspos = (34, 0, False)
     
     if self.last_abspos != (int(kwargs["piece"]), int(kwargs["location"]), kwargs["is_clockwise"]):
         #new absolute car position detected
-        if self.track_pieces == [] and int(kwargs["piece"]) == 34 and kwargs["is_clockwise"] == "False":
-            car.turn()
-            car.change_lane(400, 2000, -68)
-            self.track_pieces.append((34, "cw_1"))
-            self.track_pieces.append((33, "cw_1"))
+        if self.track_pieces == [] and int(kwargs["piece"]) == 34 and kwargs["is_clockwise"] == "True":
+            self.turn()
+            self.change_lane(400, 2000, -68)
+            self.track_pieces.append((34, 0, "cw_0"))
         elif self.track_pieces == [] and int(kwargs["piece"]) == 34 and kwargs["is_clockwise"] == "False":
-            self.track_pieces.append((34, "cw_1"))
-            self.track_pieces.append((33, "cw_1"))
-        elif (33, "cw_1") in self.track_pieces:
-            if int(kwargs["piece"]) != self.track_pieces[-1][0] or kwargs["is_clockwise"] != self.last_abspos[2]:
-                #new piece number or
-                #direction_change
-                self.track_pieces.append((int(kwargs["piece"]), clockwise_format(kwargs["is_clockwise"])))
-            elif int(kwargs["piece"]) == self.track_pieces[-1][0] and kwargs["is_clockwise"] == self.last_abspos[2]:
-                #same piece number, but new counter
-                if clockwise_format(kwargs["is_clockwise"]) == "cw_1" and int(kwargs["location"]) < self.last_abspos[1]:
-                    self.track_pieces.append((int(kwargs["piece"]), clockwise_format(kwargs["is_clockwise"])))
-                elif clockwise_format(kwargs["is_clockwise"]) == "cw_0" and int(kwargs["location"]) > self.last_abspos[1]:
-                    self.track_pieces.append((int(kwargs["piece"]), clockwise_format(kwargs["is_clockwise"])))
-            elif int(kwargs["piece"]) == 34 and len(self.track_pieces) > 3 and open(TRACK_FILE, 'r').read() != create_trackfile_insert(self.track_pieces):
+            self.track_pieces.append((34, 0, "cw_0"))
+
+        if (34, 0, "cw_0") in self.track_pieces:
+            
+            if int(kwargs["piece"]) == 34 and len(self.track_pieces) > 3 and open(TRACK_FILE, 'r').read() != create_trackfile_insert(self.track_pieces):
                 #finished first part of scan process
                 print("Finished first part of scan process: 1/2")
                 with open(TRACK_FILE, 'w') as dok:
                     dok.write(create_trackfile_insert(self.track_pieces))
                 self.track_pieces = []
                 self.progress = 50
-                car.change_speed(800, 1000)
+                self.change_speed(400, 1000)
+
             elif int(kwargs["piece"]) == 34 and len(self.track_pieces) > 3 and open(TRACK_FILE, 'r').read() == create_trackfile_insert(self.track_pieces):
                 #track_discovery_finished when discovered data matches track_file_data
                 print("Finished track discovery!")
-                car.change_speed(0, 1000)
+                self.change_speed(0, 1000)
                 del self.track_pieces, self.last_abspos
                 exit()
+
+
+            elif (int(kwargs["piece"]) != self.track_pieces[-1][0] or kwargs["is_clockwise"] != self.last_abspos[2]) and kwargs["piece"] != 34:
+                #new piece number or
+                #direction_change
+                self.track_pieces.append((int(kwargs["piece"]), int(kwargs["location"]), clockwise_format(kwargs["is_clockwise"])))
+
+            elif int(kwargs["piece"]) == self.track_pieces[-1][0] and kwargs["is_clockwise"] == self.last_abspos[2]:
+                if clockwise_format(kwargs["is_clockwise"]) == "cw_0" and int(kwargs["location"]) < self.last_abspos[1]:
+                    self.track_pieces.append((int(kwargs["piece"]), int(kwargs["location"]), clockwise_format(kwargs["is_clockwise"])))
+                elif clockwise_format(kwargs["is_clockwise"]) == "cw_1" and int(kwargs["location"]) > self.last_abspos[1]:
+                    self.track_pieces.append((int(kwargs["piece"]), int(kwargs["location"]), clockwise_format(kwargs["is_clockwise"])))
+                   
         self.last_abspos = (int(kwargs["piece"]), int(kwargs["location"]), kwargs["is_clockwise"])
+        
     return
 
 
 # let's drive!
-with open(TRACK_FILE, 'w') as dok:
-    dok.write("")
-car = Overdrive(args.host, args.port, args.car, discovery_driving_policy)  # init overdrive object with custom policy
-car.change_speed(400, 2000)  # set car speed with speed = 400, acceleration = 2000
-car.change_lane(400, 2000, -68)
-# the car will change speed when traversing the starting straight segment
-input()  # hold the program so it won't end abruptly
+
+try:
+    with open(TRACK_FILE, 'w') as dok:
+        dok.write("")
+    car = Overdrive(args.host, args.port, args.car, discovery_driving_policy)  # init overdrive object with custom policy
+    car.change_speed(400, 2000)  # set car speed with speed = 400, acceleration = 2000
+    car.change_lane(400, 2000, -68)
+    # the car will change speed when traversing the starting straight segment
+    input()  # hold the program so it won't end abruptly
+
+except KeyboardInterrupt:
+    car.change_speed(0, 2000)
